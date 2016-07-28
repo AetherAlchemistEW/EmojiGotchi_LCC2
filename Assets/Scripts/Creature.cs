444657c41acfb0504838a422f055f05643869db1
Edward Whitehead <ewhitehead@aetheralchemist.com.au>
2016-07-28 15:44:25 +1000

    Pseudo-Code

---
+++ Assets/Scripts/Creature.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Creature : MonoBehaviour
+{
+    //VARIABLES
+    //PoopNumber
+    public int poopNumber;
+    //Age
+    public float age;
+    //Hunger
+    public float hunger;
+    //Hygiene
+    public float hygiene;
+    //Tiredness
+    public float tiredness;
+    //IsSick
+    public bool isSick;
+    //IsAwake
+    public bool isAwake;
+
+    //stat timers
+    public float tirednessTimer, hungerTimer, poopTimer, sickTimer;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Hunger
+        //Timer--
+        //< 75 %, 
+        //Eat
+        //< 20 %, 
+        //Get Sick
+        //Poop
+        //Hunger > 50 %
+        //Timer--
+        //< 0, 
+        //Poop
+        //Reset Timer
+        //Tired
+        //Awake?
+        //Timer--
+        //< 0, Sleep
+        //Else
+        //Timer++
+        //> 100, Awake
+        //Sick
+        //Hygiene -= PoopNumber * Age
+        //Hygiene < 20 %, Get Sick
+        //IsSick?
+        //Timer --
+        //< 0, Die
+    }
+
+    //Poop
+        //Spawn Poop
+        //Increase Poop Number
+
+    //Sleep
+        //Set asleep, while asleep 
+        //run timer down until <0
+        //Set not asleep
+
+    //Eat
+        //Find food, if food move to food, once at food eat
+
+    //GetSick
+        //IsSick
+
+    //Die
+        //Spawn Headstone
+        //Destroy
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    //VARIABLES
+    //Creature Prefab
+    public GameObject creaturePrefab;
+    //Poop Prefab
+    //Food Prefab
+
+    //Creature Reference
+    private GameObject creatureRef;
+
+	// Update is called once per frame
+	void Update ()
+    {
+	    //If Creature reference is null and input spawn creature and set to reference
+        if(creatureRef == null && Input.GetKeyDown(KeyCode.Space))
+        {
+            Spawn();
+        }
+	}
+
+    //Spawn
+    void Spawn()
+    {
+        //Instantiate creature and set it to creature reference
+        creatureRef = Instantiate(creaturePrefab);
+    }
+
+    //Clean
+        //Remove all poop, reset creature poop number, reset creature hygiene
+
+    //Feed
+        //Spawn food
+
+    //Medicate
+        //Reduce creature sickness
+}
